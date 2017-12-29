da32a3c61845a7f767cf46f823f39f90b1ec4f1a
Kevin Boyette <kevin.boyette042@gmail.com>
2017-12-29 07:57:04 -0500

    Add BubbleSort

---
+++ src/sorts/bubble_sort.go
@@ -0,0 +1,23 @@
+package sorts
+
+// BubbleSort will sort a slice of ints
+// You probably shouldn't use BubbleSort in a
+// real program
+func BubbleSort(list []int) []int {
+	listLength := len(list)
+	if listLength <= 0 {
+		return list
+	}
+	for i := 1; i < listLength; i++ {
+		for j := 0; j < listLength-1; j++ {
+			if list[j] > list[j+1] {
+				temp := list[j]
+				list[j] = list[j+1]
+				list[j+1] = temp
+			}
+		}
+
+	}
+
+	return list
+}
---
+++ src/sorts/bubble_sort_test.go
@@ -0,0 +1,39 @@
+package sorts
+
+import "testing"
+
+func TestBubbleSort(t *testing.T) {
+	testTable := []struct {
+		testName  string
+		testParam []int
+		expected  []int
+	}{
+		{"testing BubbleSort([]int{})", []int{}, []int{}},
+		{"testing BubbleSort([]int{1})", []int{1}, []int{1}},
+		{"testing BubbleSort([]int{5,4,3,2,1})", []int{5, 4, 3, 2, 1}, []int{1, 2, 3, 4, 5}},
+		{"testing BubbleSort([]int{2,1,3,5,4})", []int{2, 1, 3, 5, 4}, []int{1, 2, 3, 4, 5}},
+		{"testing BubbleSort([]int{1,2,3,4,5})", []int{1, 2, 3, 4, 5}, []int{1, 2, 3, 4, 5}},
+	}
+
+	for _, test := range testTable {
+		t.Run(test.testName, func(t *testing.T) {
+			actual := BubbleSort(test.testParam)
+			expected := test.expected
+			testCase := true
+			for index := range actual {
+				// This if can result in panic: index out of range
+				if actual[index] != expected[index] {
+					testCase = false
+				}
+			}
+			if testCase != true {
+				t.Errorf("During %s; expected %v and got %v",
+					test.testName,
+					test.expected,
+					actual,
+				)
+			}
+		})
+	}
+
+}
