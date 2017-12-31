c26b47e1326f9d257a07c6c71a791074248f5589
Kevin Boyette <kevin.boyette042@gmail.com>
2017-12-31 09:04:06 -0500

    Add Test Cases For Two Slice Params In Sorts

--- src/sorts/merge_sort_test.go
+++ src/sorts/merge_sort_test.go
@@ -12,36 +12,6 @@ func TestMergeSort(t *testing.T) {
 }
 
 func TestMerge(t *testing.T) {
-	cases := map[string]struct {
-		testParamX []int
-		testParamY []int
-		expected   []int
-	}{
-		"Merge([]int{}, []int{})":            {[]int{}, []int{}, []int{}},
-		"Merge([]int{1,2}, []int{3,4,5})":    {[]int{1, 2}, []int{3, 4, 5}, []int{1, 2, 3, 4, 5}},
-		"Merge([]int{1}, []int{})":           {[]int{1}, []int{}, []int{1}},
-		"Merge([]int{2,0,3},[]int{-1, -2})":  {[]int{2, 0, 3}, []int{-1, -2}, []int{-1, -2, 2, 0, 3}},
-		"Merge([]int{2,0,3},[]int{-1,0 -2})": {[]int{2, 0, 3}, []int{-1, 0, -2}, []int{-1, 0, -2, 2, 0, 3}},
-	}
-	for name, tc := range cases {
-		t.Run(name, func(t *testing.T) {
-			actual := sorts.Merge(tc.testParamX, tc.testParamY)
-			expected := tc.expected
-			testCase := true
-			for index := range actual {
-				// This if can result in panic: index out of range
-				if actual[index] != expected[index] {
-					testCase = false
-				}
-			}
-			if testCase != true {
-				t.Errorf("During %s; expected %v and got %v",
-					name,
-					tc.expected,
-					actual,
-				)
-			}
-		})
-	}
-
+	cases := twoParamTestTable()
+	runTestsTwoParams(sorts.Merge, cases, t)
 }
