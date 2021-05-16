e314dfb2c73a27781cce2760efd6ba3ece8666d1
Kevin Boyette <kevinxboyette@gmail.com>
2021-05-15 22:02:43 -0400

    feat: OldestTwo - Replace Naive Algorithm

--- misc/two_oldest.go
+++ misc/two_oldest.go
@@ -1,12 +1,15 @@
 package misc
 
-import "sort"
-
 // TwoOldestAges returns the two integer ages that are greatest.
 // This is another CodeWars problem.
 func TwoOldestAges(ages []int) [2]int {
-	// TODO: Use the QuickSelect algorithm
-	sort.Ints(ages)
-	i := len(ages) - 1
-	return [2]int{ages[i-1], ages[i]}
+	var oldestTwo [2]int
+	for _, age := range ages {
+		if age > oldestTwo[1] {
+			oldestTwo[0], oldestTwo[1] = oldestTwo[1], age
+		} else if age > oldestTwo[0] {
+			oldestTwo[0] = age
+		}
+	}
+	return oldestTwo
 }
