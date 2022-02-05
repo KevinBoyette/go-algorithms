e4280e0f0b3c3a1ec0942b51bc5782416f8a7045
Kevin Boyette <kevinxboyette@gmail.com>
2022-02-04 23:12:55 -0500

    feat: sum of unique elements

---
+++ misc/sum_of_unique.go
@@ -0,0 +1,23 @@
+package misc
+
+// SumOfUniqueElements returns the sum of the elements
+// by skipping duplicates
+//
+// https://leetcode.com/problems/sum-of-unique-elements/
+func SumOfUniqueElements(nums []int) int {
+	const max = 101
+	results := [max]int{}
+	for _, i := range nums {
+		results[i]++
+	}
+
+	total := 0
+	for i := 1; i < max; i++ {
+		if results[i] == 1 {
+			total += i
+		}
+	}
+
+	return total
+
+}
---
+++ misc/sum_of_unique_test.go
@@ -0,0 +1,40 @@
+package misc_test
+
+import (
+	unique "kevinboyette/algorithms/misc"
+	"testing"
+)
+
+func TestSumOfUnique(t *testing.T) {
+	cases := map[string]struct {
+		param    []int
+		expected int
+	}{
+		"First Case": {
+			[]int{1, 2, 3, 2},
+			4,
+		},
+		"Second Case": {
+			[]int{1, 1, 1, 1, 1},
+			0,
+		},
+		"Third Case": {
+			[]int{1, 2, 3, 4, 5},
+			15,
+		},
+	}
+
+	for name, testCase := range cases {
+		t.Run(name, func(t *testing.T) {
+			actual := unique.SumOfUniqueElements(testCase.param)
+			expected := testCase.expected
+			if actual != expected {
+				t.Errorf("During %s; expected %v and got %v",
+					name,
+					testCase.expected,
+					actual,
+				)
+			}
+		})
+	}
+}
