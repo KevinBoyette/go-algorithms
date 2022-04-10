8b8e8c5cce4f67e6a5b8febf0a19762dc5f07a43
Kevin Boyette <kevinxboyette@gmail.com>
2022-04-10 14:24:14 -0400

    chore: more test clean up

--- misc/boats_to_save_test.go
+++ misc/boats_to_save_test.go
@@ -8,35 +8,21 @@ import (
 
 func TestNumRescueBoats(t *testing.T) {
 	cases := map[string]struct {
-		people   []int
-		limit    int
-		expected int
+		people []int
+		limit  int
+		want   int
 	}{
-		"firstCase": {
-			[]int{1, 2},
-			3,
-			1,
-		},
-		"secondCase": {
-			[]int{3, 2, 2, 1},
-			3,
-			3,
-		},
-		"thirdCase": {
-			[]int{3, 5, 3, 4},
-			5,
-			4,
-		},
+		"firstCase":  {[]int{1, 2}, 3, 1},
+		"secondCase": {[]int{3, 2, 2, 1}, 3, 3},
+		"thirdCase":  {[]int{3, 5, 3, 4}, 5, 4},
 	}
 	for name, tt := range cases {
 		t.Run(name, func(t *testing.T) {
-			actual := NumRescueBoats(tt.people, tt.limit)
-			expected := tt.expected
-			if actual != expected {
+			if got := NumRescueBoats(tt.people, tt.limit); got != tt.want {
 				t.Errorf("During %s; expected %v and got %v",
 					name,
-					expected,
-					actual,
+					tt.want,
+					got,
 				)
 			}
 
